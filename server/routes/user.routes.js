e35b79e869103bbe71285e5c1080cd5050578b44
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-05-30 09:37:35 +0200

    Apply eslint

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,28 +1,28 @@
-const { authJwt } = require("../middlewares");
-const controller = require("../controllers/user.controller");
+const { authJwt } = require('../middlewares');
+const controller = require('../controllers/user.controller');
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use((req, res, next) => {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept',
     );
     next();
   });
 
-  app.get("/api/bakery/all", controller.allAccess);
+  app.get('/api/bakery/all', controller.allAccess);
 
-  app.get("/api/bakery/user", [authJwt.verifyToken], controller.userBoard);
+  app.get('/api/bakery/user', [authJwt.verifyToken], controller.userBoard);
 
   app.get(
-    "/api/bakery/moderator",
+    '/api/bakery/moderator',
     [authJwt.verifyToken, authJwt.isModerator],
-    controller.moderatorBoard
+    controller.moderatorBoard,
   );
 
   app.get(
-    "/api/bakery/admin",
+    '/api/bakery/admin',
     [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
+    controller.adminBoard,
   );
 };
