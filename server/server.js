40c56416e9418325f7b66294493a38c5914af487
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-06-01 14:41:55 +0200

    Add product route

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.get('/', (req, res) => {
 
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
+require('./routes/product.routes')(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
