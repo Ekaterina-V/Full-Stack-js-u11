34a82ae7928000f5ab47b1d88d7ae11174eabee9
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-06-08 20:05:01 +0200

    Move the products output from homepage to user page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { ProductService } from '../services/product.service';
-import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +7,9 @@ import { Product } from '../models/product.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  response?: Product[];
 
-  constructor(private userService: UserService, private productService: ProductService) {}
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(
-      (data: any) => {
-        this.response = data;
-        console.log(this.response);
-      }
-    );
   }
 }
