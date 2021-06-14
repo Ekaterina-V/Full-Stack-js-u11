e75906536d3fbb679b6570fe779ad61ee771e72a
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-06-14 17:01:17 +0200

    Install service worker

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { BoardUserComponent } from './board-user/board-user.component';
 
 import { authInterceptorProviders } from './helpers/auth.interceptor';
 import { CartComponent } from './cart/cart.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,13 @@ import { CartComponent } from './cart/cart.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
